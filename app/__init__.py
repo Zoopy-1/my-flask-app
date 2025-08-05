298ab76f91bd0354878909663d96f765ea74e10e
zhaojunhui <zhaojunhui@zhaojunhuideMacBook-Air.local>
2025-08-05 21:06:56 +0800

    refactor: Implement app factory, separate models

---
+++ app/__init__.py
@@ -0,0 +1,31 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from config import Config
+import os
+
+db = SQLAlchemy()
+
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+
+    # Ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    db.init_app(app)
+
+    from app.main import bp as main_bp
+    app.register_blueprint(main_bp)
+
+    from app.auth import bp as auth_bp
+    app.register_blueprint(auth_bp, url_prefix='/auth')
+
+    # This is a good place to create DB tables if not using migrations
+    with app.app_context():
+        db.create_all()
+
+    return app
